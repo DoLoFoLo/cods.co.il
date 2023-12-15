46aecc30c525bdd2a5b5c920068106375c142c3a
codes.co.il <153930672+DoLoFoLo@users.noreply.github.com>
2023-12-15 17:19:02 +0200

    Add files via upload

---
+++ dan.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Welcome to My school 
+pause
+@echo off
+setlocal enabledelayedexpansion
+
+echo in my school you will found a biggest classes and Soccer fields and lots of space during breaks and lots of other things!
+
+echo.
+
+echo goodbye see you later!
+
+
+set /p userInput=Enter a number and  Click Enter to: CLOSE THE APP
